39e78d2c1146dd6a84bb5284640faac77b3597e6
Jaansi Parsa <jaansi@jointaro.com>
2026-03-24 07:41:41 -0700

    new news articlce

--- components/blog/InTheNews.tsx
+++ components/blog/InTheNews.tsx
@@ -38,9 +38,21 @@ const defaultNewsItems: NewsItem[] = [
     title: 'The Fast Track to Being a CEO Engineer',
     link: 'https://www.bloomberg.com/news/articles/2019-03-08/the-fast-track-to-being-a-ceo-engineer',
   },
+  {
+    logo: 'https://upload.wikimedia.org/wikipedia/commons/e/e8/NYSE_Logo_2022.svg',
+    date: 'March 24, 2026',
+    title: 'Prof. Chaudhari on how M.E.T. prepares students in the AI age',
+    link: 'https://www.thecube.net/events/nyse/moe-series/content/Videos/c6f6b2c1-afc1-471c-90c9-30bdab72d828',
+  },
 ]
 
 export function InTheNews({ newsItems = defaultNewsItems }: InTheNewsProps) {
+  const sortedNewsItems = [...newsItems].sort((a, b) => {
+    const dateA = new Date(a.date).getTime()
+    const dateB = new Date(b.date).getTime()
+    return dateB - dateA
+  })
+
   return (
     <section className="py-8 md:py-10">
       <div className="container mx-auto px-4">
@@ -50,7 +62,7 @@ export function InTheNews({ newsItems = defaultNewsItems }: InTheNewsProps) {
           </h2>
         </ScrollReveal>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6 md:gap-8 max-w-5xl mx-auto">
-          {newsItems.map((item, index) => (
+          {sortedNewsItems.map((item, index) => (
             <ScrollReveal key={index}>
               <NewsCard
                 logo={item.logo}
